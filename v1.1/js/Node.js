9e64dac57c226eddb52c88fe38ebced090608c5d
semirtat <semirtatlidil@gmail.com>
2024-03-11 18:30:48 -0400

    Update Node.js

--- v1.1/js/Node.js
+++ v1.1/js/Node.js
@@ -254,7 +254,7 @@ function Node(model, config){
 			width = ctx.measureText(self.label).width;
 		}
 		*/
-		ctx.fillText(self.label, 0, 100);
+		ctx.fillText(self.label, -50, 100);
 
 		// WOBBLE CONTROLS
 		var cl = 40;
